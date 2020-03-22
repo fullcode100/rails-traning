820aabce4a85ac82d0e79ce258532d9bedb2739a
fullcode100 <snowleopard722@outlook.com>
2020-03-22 14:23:15 -0300

    client and stock validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :category
   validates :name, :stock, presence: :true
-
+  validates_numericality_of :stock, :greater_than_or_equal_to => 0
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :books
   resources :categories
   resources :authors
